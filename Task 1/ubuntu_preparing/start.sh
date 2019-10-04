f33151b0cc0d71bc976415579caa48e264362026
rashpil84 <55686486+rashpil84@users.noreply.github.com>
2019-10-04 16:08:22 +0300

    Update start.sh

--- Task 1/ubuntu_preparing/start.sh
+++ Task 1/ubuntu_preparing/start.sh
@@ -1,7 +1,1 @@
-#/bin/bash
-echo
-echo -e "################################################################"
-echo -e "#		Web Hosting implementation Script	       #"
-echo -e "#		Written By: Vadim Bykau			       #"
-echo -e "#		Updated: 19.09.2019			       #"
-echo -e "################################################################"
+
